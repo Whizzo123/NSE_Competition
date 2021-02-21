dcbfb6373443e3970e2ab8b6ce4bdc3049b8005f
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2021-02-21 19:01:18 +0000

    Prototype Complete
    A VERY Basic prototype is complete. I have made a prototype folder fill with all assets and scripts I used for the level. I have also made a prototype scene.
    A worry of mine is that it has deleted
    "Assets\Photon-Bolt\Photon\PhotonBolt\packages\bolt_samples.unitypackage.meta". I have no idea what that is and I have not deliberately deleted anything that I have received from the main branch.

---
+++ Assets/Prototype Assets/Proto_Hit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proto_Hit : MonoBehaviour
+{
+    public List<GameObject> hitObjects;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit with machete");
+        if (other.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Hit obstacle");
+            //Destroy(other.gameObject);
+            hitObjects.Add(other.gameObject);
+        }
+    }
+}
