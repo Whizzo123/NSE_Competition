6d2cea9221c03853dd38dd1d26996a47a812813a
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2021-03-18 20:14:20 +0000

    options

--- Assets/Scripts/MenuButtonManagerUI.cs
+++ Assets/Scripts/MenuButtonManagerUI.cs
@@ -5,10 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonManagerUI : MonoBehaviour
 {
-    
+    public GameObject panel1;
+    public GameObject panel2;
+
     public void Play()
     {
         SceneManager.LoadScene("LobbyScene");
+    }
+
+    public void Options()
+    {
+        panel1.SetActive(true);
+        panel2.SetActive(false);
+    }
+
+    public void titleScreen()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(true);
+    }
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene("TitleScene");
     }
 
     public void Quit()
