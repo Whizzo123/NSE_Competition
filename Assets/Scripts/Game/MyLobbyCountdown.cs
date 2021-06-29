860c64ce0305cb1b553c1eb6a3e48c942faf06d3
Whizzo123 <1900340@uad.ac.uk>
2021-06-29 13:09:06 +0100

    Added in LobbyCountdown functionality

---
+++ Assets/Scripts/Game/MyLobbyCountdown.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyLobbyCountdown : NetworkBehaviour
+{
+    [Header("Variables")]
+    public float countdownTime;
+    private float currentCountdownTime;
+
+    // Update is called once per frame
+    void Start()
+    {
+        currentCountdownTime = countdownTime;
+        StartCoroutine(Countdown());
+    }
+
+    [Server]
+    private IEnumerator Countdown()
+    {
+        Debug.Log("InsideCountdown");
+        var floorTime = Mathf.FloorToInt(countdownTime);
+
+        LoadoutCountdown countdown;
+        while (currentCountdownTime > 0)
+        {
+            yield return null;
+
+            currentCountdownTime -= Time.deltaTime;
+            var newFloorTime = Mathf.FloorToInt(currentCountdownTime);
+
+            if (newFloorTime != floorTime)
+            {
+                floorTime = newFloorTime;
+
+                UpdateCountdown(floorTime);
+            }
+        }
+        DisableLoadoutScreen();
+    }
+
+    [ClientRpc]
+    private void DisableLoadoutScreen()
+    {
+        FindObjectOfType<AbilitySlotBarUI>().LoadInAbilitiesFromLoadout(FindObjectOfType<LoadoutBarUI>().GetLoadoutForAbilitySlotBar());
+        FindObjectOfType<CanvasUIManager>().loadoutScreen.SetActive(false);
+        //TODO:- Somehow let local player know that the loadout has been released
+    }
+
+    [ClientRpc]
+    private void UpdateCountdown(float time)
+    {
+        FindObjectOfType<CanvasUIManager>().loadoutTimeText.text = "" + time;
+    }
+}
